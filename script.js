31482cfa10c7d98957658211d4316ba4a671d8ba
draw-de <57506579+draw-de@users.noreply.github.com>
2020-02-19 03:18:13 -0500

    local save & images

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
 var apiKey = "8759341f015b5fa1502e0bb2d6da1061";
 
-// Current weather
-var weatherURL =
-  "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=" +
-  apiKey;
-
-$.ajax({
-  url: weatherURL,
-  method: "GET"
-}).then(function(obj) {
-  console.log(obj);
-
-  var name = obj.name;
-  var temp = obj.main.temp;
-  var humidity = obj.main.humidity;
-  var wind = obj.wind.speed;
-
-  $("#cityName").html(name);
-  $("#temp").html(temp + " °F");
-  $("#humid").html(humidity + "%");
-  $("#windSpeed").html(wind + " mph");
-
-  $(this).each(function() {
-    var coord = obj.coord;
-    uvIndex(coord);
-  });
-});
-
-// 5 day Forecast
-
-var forecastURL =
-  "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?q=miami&appid=" +
-  apiKey;
-
-$.ajax({
-  url: forecastURL,
-  method: "GET"
-}).then(function(obj) {
-  console.log(obj);
-});
-
-// Current UV Index
-function uvIndex(coords) {
-  var lon = coords.lon;
-  var lat = coords.lat;
-
-  var url =
-    "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/uvi?lat=" +
-    lat +
-    "&lon=" +
-    lon +
-    "&appid=" +
+$(".cityBtn").on("click", function() {
+  // Current weather
+  var cityInput = $("#cityInput").val();
+  var weatherURL =
+    "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityInput +
+    "&units=imperial&appid=" +
     apiKey;
 
   $.ajax({
-    url: url,
+    url: weatherURL,
     method: "GET"
   }).then(function(obj) {
-    var uv = obj.value;
-    $("#uvIndex").html(uv);
+    console.log(obj);
+
+    var name = obj.name;
+    var temp = obj.main.temp;
+    var humidity = obj.main.humidity;
+    var wind = obj.wind.speed;
+    var icon =
+      "http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png";
+
+    $("#cityName").html(name);
+    $("#cityName").append(
+      "<img class='imageSave' id='image' src='" + icon + "'>"
+    );
+    $("#temp").html("Temperature: " + temp + " °F");
+    $("#humid").html("Humidity: " + humidity + "%");
+    $("#windSpeed").html("Wind speed: " + wind + " mph");
+
+    $(this).each(function() {
+      var coord = obj.coord;
+      uvIndex(coord);
+    });
+    $(".save").each(function() {
+      var id = $(this).attr("id");
+      var value = $(this).text();
+      localStorage.setItem(id, value);
+    });
+    $(".imageSave").each(function() {
+      var id = $(this).attr("id");
+      var value = $(this).attr("src");
+      localStorage.setItem(id, value);
+    });
   });
-}
+
+  function uvIndex(coords) {
+    var lon = coords.lon;
+    var lat = coords.lat;
+    // Current UV Index
+    var urlIndex =
+      "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      apiKey;
+
+    $.ajax({
+      url: urlIndex,
+      method: "GET"
+    }).then(function(obj) {
+      var uv = obj.value;
+      $("#uvIndex").html("UV Index: " + uv);
+    });
+    $(".save").each(function() {
+      var id = $(this).attr("id");
+      var value = $(this).text();
+      localStorage.setItem(id, value);
+    });
+  }
+});
+
+$(".save").each(function() {
+  var id = $(this).attr("id");
+  $(this).html(localStorage.getItem(id));
+});
